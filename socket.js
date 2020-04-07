c7d08abca81f4f42b57385a5865bf66139d947b6
hys <jericoh@naver.com>
2020-04-07 23:57:22 +0900

    socket.io

--- socket.js
+++ socket.js
@@ -27,12 +27,13 @@
 const SocketIO =require('socket.io')
 
 module.exports=(server)=>{
-  const io=SocketIO(server, {path : 'socket.io'});
+  const io=SocketIO(server, {path : '/socket.io'});
 
   io.on('connection', (socket)=>{
     const req=socket.request;
     const ip=req.headers['x-forwarded-for'] || req.connection.remoteAddress; //클라이언트의 IP를 알아내는 방법
     console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
+    
     socket.on('disconnect', ()=>{
       console.log('클라이언트 접속 해제', ip, socket.id);
       clearInterval(socket.interval);
