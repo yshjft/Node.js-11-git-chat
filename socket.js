da03d1feb69b080c1e44ea4c4f450cbb6eedb486
hys <jericoh@naver.com>
2020-04-10 20:39:57 +0900

    11.4 socket.js

--- socket.js
+++ socket.js
@@ -26,26 +26,52 @@
 // };
 const SocketIO =require('socket.io')
 
-module.exports=(server)=>{
-  const io=SocketIO(server, {path : '/socket.io'});
+// module.exports=(server)=>{
+//   const io=SocketIO(server, {path : '/socket.io'});
 
-  io.on('connection', (socket)=>{
-    const req=socket.request;
-    const ip=req.headers['x-forwarded-for'] || req.connection.remoteAddress; //클라이언트의 IP를 알아내는 방법
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
+//   io.on('connection', (socket)=>{
+//     const req=socket.request;
+//     const ip=req.headers['x-forwarded-for'] || req.connection.remoteAddress; //클라이언트의 IP를 알아내는 방법
+//     console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
     
+//     socket.on('disconnect', ()=>{
+//       console.log('클라이언트 접속 해제', ip, socket.id);
+//       clearInterval(socket.interval);
+//     });
+//     socket.on('error', (error)=>{
+//       console.error(error);
+//     });
+//     socket.on('reply', (data)=>{
+//       console.log(data);
+//     });
+//     socket.interval=setInterval(()=>{
+//       socket.emit('news', 'Hello Socket.io');
+//     }, 3000);
+//   });
+// };
+
+module.exports=(server, app)=>{
+  const io=SocketIO(server, {path : '/socket.io'});
+  app.set('io', io);
+  const room=io.of('/room');
+  const chat=io.of('/chat');
+  room.on('connection', (socket)=>{
+    console.log('room 네임스페이스에 접속');
     socket.on('disconnect', ()=>{
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
+      console.log('room 네임스페이스 접속 해제');
     });
-    socket.on('error', (error)=>{
-      console.error(error);
+  });
+  chat.on('connection', (socket)=>{
+    console.log('chat 네임스페이스에 접속');
+    const req=socket.request;
+    const {headers: {referer}}=req;
+    const roomId=referer
+      .split('/')[referer.split('/').length-1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
+    socket.on('disconnect', ()=>{
+      console.log('chat 네임스페이스 접속 해제');
+      soeckt.leave(roomId);
     });
-    socket.on('reply', (data)=>{
-      console.log(data);
-    });
-    socket.interval=setInterval(()=>{
-      socket.emit('news', 'Hello Socket.io');
-    }, 3000);
   });
 };
